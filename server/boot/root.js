475ba899ed51a1852fcbfe2f67e7c50892cd26bf
Andy Ngo <ngotrieu.giaan@gmail.com>
2018-06-29 00:58:47 -0700

    Fix ACLs

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,27 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+  
+  var path = require('path');
+  router.get('/image/avatar/:name',
+    function (req, res, next) {
+      // do something with req.user
+      res.sendFile(path.resolve('./storage/avatar/'+req.params.name), function(err){
+        if(err)
+          res.status(404).send('File not found');
+      });
+    }
+  );
+
+  router.get('/image/news/:name',
+    function (req, res, next) {
+      // do something with req.user
+      res.sendFile(path.resolve('./storage/news/'+req.params.name), function(err){
+        if(err)
+          res.status(404).send('File not found');
+      });
+    }
+  );
+
   server.use(router);
 };
